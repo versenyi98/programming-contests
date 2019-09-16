e5e90fe48165d5018955e30dfb78dfe19eb81fff
Péter Versényi <versenyi98@gmail.com>
2019-09-16 20:38:46 +0200

    Add faster Ancient Messages

--- ICPC/5130-Ancient-Messages/main.cpp
+++ ICPC/5130-Ancient-Messages/main.cpp
@@ -4,62 +4,37 @@
 
 using namespace std;
 
-void floodFill(int img[][200], int brush, int i, int j, int height, int width, int target) {
+int img[200][200];
+int height, width;
 
-  if (img[i][j] == brush || img[i][j] != target || i < 0 || j < 0 || i >= height || j >= width) return;
+void floodFill(int brush, int i, int j, int source) {
+
+  if (i < 0 || j < 0 || i >= height || j >= width * 4|| img[i][j] != source)  return;
     img[i][j] = brush;
 
-    floodFill(img, brush, i + 1, j, height, width, target);
-    floodFill(img, brush, i - 1, j, height, width, target);
-    floodFill(img, brush, i, j + 1, height, width, target);
-    floodFill(img, brush, i, j - 1, height, width, target);
-
-}
-
-int findNeighbour(int img[][200], int i, int j, int di, int dj, int height, int width) {
-
-	while (true) {
-
-		if (i < 0 || j < 0 || i >= height || j >= width) break;
-
-		if (img[i][j] > 0) {
-			return img[i][j];
-		}
-
-		i += di;
-		j += dj;
-	}
-
-	return -2;
+    floodFill(brush, i + 1, j, source);
+    floodFill(brush, i - 1, j, source);
+    floodFill(brush, i, j + 1, source);
+    floodFill(brush, i, j - 1, source);
 }
 
 int main() {
 
-  char holesToSign[6];
+  char holesToSign[6] = {'W', 'A', 'K', 'J', 'S', 'D' };
 
-  holesToSign[0] = 'W';
-  holesToSign[1] = 'A';
-  holesToSign[2] = 'K';
-  holesToSign[3] = 'J';
-  holesToSign[4] = 'S';
-  holesToSign[5] = 'D';
-
-  int n, m;
   int number = 0;
-  int img[200][200];
 
   while (true) {
-    cin >> n >> m;
+    cin >> height >> width;
 
     number++;
 
-    if (n == 0 && m == 0) {
+    if (height == 0 && width == 0) {
       return 0;
     }
 
-//Handle input
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
 
         int l = 8;
         int hexa;
@@ -73,53 +48,67 @@ int main() {
       }
     }
 
-//Flood signs to different colors
+
     int brush = 2;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m * 4; j++) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width * 4; j++) {
         if (img[i][j] == 1) {
-          floodFill(img, brush, i, j, n, m * 4, 1);
+          floodFill(brush, i, j, 1);
           brush++;
         }
       }
     }
 
-    int holes[brush - 2];
-    for (int i = 0; i < brush - 2; i++) {
+    int holes[brush];
+    for (int i = 0; i < brush; i++) {
       holes[i] = 0;
     }
-
-//Flood background
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m * 4; j++) {
-        if (img[i][j] == 0) {
-
-          int a = findNeighbour(img, i, j, +1, 0, n, m * 4);
-          int b = findNeighbour(img, i, j, -1, 0, n, m * 4);
-          int c = findNeighbour(img, i, j, 0, +1, n, m * 4);
-          int d = findNeighbour(img, i, j, 0, -1, n, m * 4);
-
-          if (a < 0 || b < 0 || c < 0 || d < 0 || a != b || b != c || c != d) {
-            floodFill(img, -1, i, j, n, m * 4, 0);
-          }
-        }
+/*
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width * 4; j++) {
+        cout << img[i][j];
       }
+      cout << endl;
     }
-//Count holes for each sign
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m * 4; j++) {
+*/
+    for (int i = 0; i < width * 4; i++) {
 
+      if (img[0][i] == 0) {
+        floodFill(1, 0, i, 0);
+      }
+
+      if (img[height - 1][i] == 0) {
+        floodFill(1, height - 1, i, 0);
+      }
+
+    }
+
+    for (int i = 0; i < height; i++) {
+
+      if (img[i][0] == 0) {
+        floodFill(1, i, 0, 0);
+      }
+
+      if (img[i][width * 4 - 1] == 0) {
+        floodFill(1, i, width * 4 - 1, 0);
+      }
+
+    }
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width * 4; j++) {
         if (img[i][j] == 0) {
-          int a = findNeighbour(img, i, j, 0, -1, n, m * 4);
-	          holes[a - 2]++;
-          floodFill(img, a, i, j, n, m * 4, 0);
+          int color = img[i][j - 1];
+
+          holes[color]++;
+          floodFill(color, i, j, 0);
         }
       }
     }
 
     string return_val = "";
 
-    for (int i = 0; i < brush - 2; i++) {
+    for (int i = 2; i < brush; i++) {
       return_val += holesToSign[holes[i]];
     }
 

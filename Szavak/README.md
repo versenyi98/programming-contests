fb102e8ee09329556271737dd41caef1b520d520
Péter Versényi <versenyi98@gmail.com>
2019-10-22 00:18:52 +0200

    Add Szavak

---
+++ Szavak/README.md
@@ -0,0 +1,1 @@
+https://www.oktatas.hu/pub_bin/dload/kozoktatas/tanulmanyi_versenyek/oktv/oktv2018_2019_2ford/info2_flap2f_oktv_1819.pdf
---
+++ Szavak/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+map <char, int> szabad;
+map <char, int> felhasznalt;
+
+int n;
+string szo;
+string betuk;
+
+string kovetkezo() {
+    map <char, int> szabad_cp = szabad;
+
+// Nem teljes szó következője 
+    if (szo.length() != n) {
+        for (auto i : szabad_cp) {
+            if (i.second) {
+                return szo + i.first;
+            }
+        }
+    }
+
+// Teljes szó következője
+    for (int i = n - 1; i >= 1; i--) {
+        if (szo[i] <= szo[i - 1]) {
+            szabad_cp[szo[i]]++;
+        } else {
+            szabad_cp[szo[i]]++;
+            for (auto j : szabad_cp) {
+                if (j.second && j.first > szo[i - 1]) {
+                    return szo.substr(0, i - 1) + j.first;
+                }
+            }
+        }
+    }
+
+// Utolsó szó következője az első szó
+    for (auto i : szabad_cp) {
+        if (i.second) {
+            return string(1, i.first);
+        }
+    }
+}
+
+string elozo() {
+    map <char, int> szabad_cp = szabad;
+
+// Teljes szó előzője
+    if (szo.length() == n) {
+        return szo.substr(0, szo.length() - 1);
+    }
+
+    char levagott = szo[szo.length() - 1];
+    szo = szo.substr(0, szo.length() - 1);
+    szabad_cp[levagott]++;
+
+// Nem teljes szó előzője
+    for (auto i : szabad_cp) {
+        if (i.second && i.first < levagott) {
+            szabad_cp[i.first]--;
+            string ret = szo + string(1, i.first);
+
+            while (true) {
+                char utolso = ' ';
+                for (auto j : szabad_cp) {
+                    if (j.second) utolso = j.first;
+                }
+                if (utolso == ' ') return ret;
+                ret += utolso;
+                szabad_cp[utolso]--;
+            }    
+        }
+    }
+
+    if (szo.length() != 0) return szo;
+    
+// Első szó előzője az utolsó szó
+    string ret = "";
+    while (true) {
+        char utolso = ' ';
+        for (auto j : szabad_cp) {
+            if (j.second) utolso = j.first;
+        }
+        if (utolso == ' ') return ret;
+        ret += utolso;
+        szabad_cp[utolso]--;
+    }    
+}
+
+int main() {
+
+    cin >> n >> betuk >> szo;
+
+    for (int i = 0; i < n; i++) {
+        szabad[betuk[i]]++;
+    }
+
+    for (int i = 0; i < szo.length(); i++) {
+        felhasznalt[szo[i]]++;
+        szabad[szo[i]]--;
+    }
+
+    cout << elozo() << endl << kovetkezo() << endl;
+
+}

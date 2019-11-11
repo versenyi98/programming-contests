5e9b8dd988e603190ce0e1d0bdb5002196f368f5
Péter <versenyi98@gmail.com>
2019-11-11 22:33:21 +0100

    Solve Házépítés

---
+++ Házépítés/README.md
@@ -0,0 +1,1 @@
+https://www.oktatas.hu/pub_bin/dload/kozoktatas/tanulmanyi_versenyek/oktv/oktv2016_2017_donto/info2_flap_d_oktv_1617.pdf
--- Házépítés/main.cpp
+++ Házépítés/main.cpp
@@ -6,86 +6,103 @@
 
 using namespace std;
 
-map <int, vector<int>> elotte_van;
-map <int, vector<int>> utana_van;
+map <int, vector<int>> megelozo;
+map <int, vector<int>> rakovetkezo;
 
 bool erintve[10000];
 
 stack<int> b_utani_munkak;
 queue<int> a_elotti_munkak;
+queue<int> b_elotti_munkak;
+queue<int> maradek;
 
-int szamol_utana(int n) {
+int munkak, megelozesek, a_munka, b_munka;
 
-	if (erintve[n - 1]) return 0;
+int mennyi_elotte(int n, int bound, queue<int>& q) {
+
+	if (n == bound) return -1;
+	if (erintve[n - 1] && n != b_munka) return 0;
 
 	erintve[n - 1] = true;
 	int count = 0;
 
-	for (int i = 0; i < utana_van[n].size(); i++) {
-		count += szamol_utana(utana_van[n][i]);
+	for (int i = 0; i < megelozo[n].size(); i++) {
+		count += mennyi_elotte(megelozo[n][i], bound, q);
 	}
 
-	a_elotti_munkak.push(n);
+	if (n == b_munka) return count;
+	q.push(n);
 	return 1 + count;
 }
 
-int szamol_elotte(int n) {
+void kezel_maradek(int n) {
+
+	if (erintve[n - 1]) return;
+	erintve[n - 1] = true;
+
+	for (int i = 0; i < megelozo[n].size(); i++) {
+		kezel_maradek(megelozo[n][i]);
+	}
+
+	maradek.push(n);
+}
+
+int mennyi_utana(int n) {
 
 	if (erintve[n - 1]) return 0;
 
 	erintve[n - 1] = true;
 	int count = 0;
 
-	for (int i = 0; i < elotte_van[n].size(); i++) {
-		count += szamol_elotte(elotte_van[n][i]);
+	for (int i = 0; i < rakovetkezo[n].size(); i++) {
+		count += mennyi_utana(rakovetkezo[n][i]);
 	}
 
 	b_utani_munkak.push(n);
 	return 1 + count;
 }
 
-int varosok, megelozesek, a_munka, b_munka;
-
 int main() {
 
-	cin >> varosok >> megelozesek >> a_munka >> b_munka;
+	cin >> munkak >> megelozesek >> a_munka >> b_munka;
 
 	while (megelozesek--) {
 		int x, y;
 		cin >> x >> y;
-		elotte_van[y].push_back(x);
-		utana_van[x].push_back(y);
+		megelozo[y].push_back(x);
+		rakovetkezo[x].push_back(y);
 	}
 
-/*
-	for (auto i : elotte_van) {
-		cout << i.first << ":";
-		for (auto j : i.second) {
-			cout << j << " ";
-		}
-		cout << endl;
+	int a_elott = mennyi_elotte(a_munka, -1, a_elotti_munkak);
+	int b_utan = mennyi_utana(b_munka);
+	int b_elott = mennyi_elotte(b_munka, a_munka, b_elotti_munkak);
+
+	for (int i = 0; i < munkak; i++) {
+		kezel_maradek(i + 1);
 	}
 
-	cout << endl;
-
-	for (auto i : utana_van) {
-		cout << i.first << ":";
-		for (auto j : i.second) {
-			cout << j << " ";
-		}
-		cout << endl;
-	}
-*/
-	int a_elott = szamol_elotte(a_munka);
-	int b_utan = szamol_utana(b_munka);
-
+	cout << munkak - a_elott - b_utan << endl;
+	bool ws = false;
 	while (!a_elotti_munkak.empty()) {
-		cout << a_elotti_munkak.front() << endl;
+		if (ws) cout << " ";
+		cout << a_elotti_munkak.front();
 		a_elotti_munkak.pop();
+		ws = true;
 	}
-	cout << endl;
+
+	while (!b_elotti_munkak.empty()) {
+		cout << " " << b_elotti_munkak.front();
+		b_elotti_munkak.pop();
+	}
+
+	while (!maradek.empty()) {
+		cout << " " << maradek.front();
+		maradek.pop();
+	}
+
 	while (!b_utani_munkak.empty()) {
-		cout << b_utani_munkak.top() << endl;
+		cout << " " << b_utani_munkak.top();
 		b_utani_munkak.pop();
 	}
+	cout << endl;
 }

0c0ecc335498c03a0cf1023e63490f05733afec5
Péter Versényi <versenyi98@gmail.com>
2019-11-25 18:27:42 +0100

    Fix Train Tracks

--- Train Tracks/main.cpp
+++ Train Tracks/main.cpp
@@ -11,15 +11,13 @@ int main() {
 	string str;
 	getline(cin, str);
 
-	bool first_ = true;
-
 	for (int i = 0; i < n; i++) {
 		getline(cin, str);
 
-		int count = 0;
-		int F1 = 0, F2 = 0, M1 = 0, M2 = 0;
+		int MM = 0, FF = 0, MF = 0, FM = 0;
+		char first_letter;
 		bool first_ = true;
-		
+
 		for (int j = 0; j < str.length(); j++) {
 
 			if (isspace(str[j])) {
@@ -30,31 +28,41 @@ int main() {
 			if (str[j] == 'F') {
 
 				if (first_) {
-					F1++;
+					first_letter = 'F';
 				} else {
-					F2++;
-					count++;
+					if (first_letter == 'F') {
+						FF++;
+					} else {
+						MF++;
+					}
 				}
 
 				first_ = false;
 			} else if (str[j] == 'M') {
 
 				if (first_) {
-					M1++;
+					first_letter = 'M';
 				} else {
-					M2++;
-					count++;
+					if (first_letter == 'M') {
+						MM++;
+					} else {
+						FM++;
+					}
 				}
 
 				first_ = false;
 			}
 		}
-
-		if (F1 == M2 && M1 == F2 && count > 1) {
+		if (MM + FF + FM == 0 && MF > 1) {
+			cout << "LOOP\n";
+		} else if (MM + FF + MF == 0 && FM > 1) {
+			cout << "LOOP\n";
+		} else if (MM == FF && MM > 0) {
 			cout << "LOOP\n";
 		} else {
 			cout << "NO LOOP\n";
 		}
+		//cout << FF << " " << MM << " " << MF << " " << FM << endl ;
 	}
 
 	return 0;

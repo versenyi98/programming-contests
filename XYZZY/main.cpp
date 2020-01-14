a7f22da355fe78a40638c6fd4bb148edbb77175a
Péter <versenyi98@gmail.com>
2020-01-14 19:46:49 +0100

    Upgrade XYZZY 0.090sec solution to 0.000sec solution

--- XYZZY/main.cpp
+++ XYZZY/main.cpp
@@ -15,13 +15,13 @@ int main() {
         tc++;
         int cost[101];
         int distance[101];
+        bool taken[101];
         vector<int> connections[101];
-
 
         for (int i = 1; i <= n; i++) {
             distance[i] = -1000000000;
             cin >> cost[i];
-
+            taken[i] = false;
             int num;
             cin >> num;
 
@@ -33,7 +33,7 @@ int main() {
         }
 
         distance[1] = 100;
-        for (int i = 0; i <= 20000 && distance[n] < 0; i++) {
+        for (int i = 0; i <= 100 && distance[n] < 0; i++) {
             for (int j = 1; j <= n; j++) {
                 if (distance[j] <= 0) continue;
                 for (auto k : connections[j]) {
@@ -43,8 +43,33 @@ int main() {
                 }
             }
         }
+
+        queue<int> q;
+
+        for (int j = 1; j <= n; j++) {
+            if (distance[j] <= 0) continue;
+            for (auto k : connections[j]) {
+                if (distance[k] < distance[j] + cost[k]) {
+                    q.push(k);
+                }
+            }
+        }
+        bool solve = false;
+        while (q.size()) {
+            auto top = q.front();
+            q.pop();
+
+            solve = solve || (top == n);
+
+            for (auto i : connections[top]) {
+                if (!taken[i]) {
+                    taken[i] = true;
+                    q.push(i);
+                }
+            }
+        }
         
-        if (distance[n] > 0) {
+        if (solve || distance[n] > 0) {
             cout << "winnable\n";
         } else {
             cout << "hopeless\n";

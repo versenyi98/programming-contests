6091fb87e216db66d4d71513470cd3358c8931e7
Péter <versenyi98@gmail.com>
2020-01-14 23:24:47 +0100

    Add check_readme.sh

---
+++ check_readme.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+set -eu
+
+DIRECTORY=$1
+
+for dir in $(ls $DIRECTORY)
+do
+    if [[ -d $dir ]]
+    then
+        cd $dir
+        echo -e "$dir" ' \t ' "$(cat README.md)"
+        cd ..
+    fi
+done

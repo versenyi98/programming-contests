ef66dbae332e36b8575af95a2afba9a20de813a9
Péter <versenyi98@gmail.com>
2020-01-17 21:42:11 +0100

    Update Trainsorting to UVa

--- Trainsorting/main.cpp
+++ Trainsorting/main.cpp
@@ -1,54 +1,36 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int ascending[2][2000], decreasing[2][2000];
-int n;
-
-void magic_function() {
-
-    ascending[1][n - 1] = 1;
-    decreasing[1][n - 1] = 1;
-
-    for (int i = n - 2; i >= 0; i--) {
-        int max_asc = 0;
-        int max_decr = 0;
-
-        for (int j = i + 1; j < n; j++) {
-            if (ascending[0][i] > ascending[0][j]) {
-                max_asc = max(max_asc, ascending[1][j]);
-            }
-            if (decreasing[0][i] < decreasing[0][j]) {
-                max_decr = max(max_decr, decreasing[1][j]);
-            }
-        }
-        ascending[1][i] = max_asc + 1;
-        decreasing[1][i] = max_decr + 1;
-
-    }
-}
-
-
 int main() {
 
-    cin >> n;
+    int n, tt;
+    cin >> tt;
 
+    for (int t = 0; t < tt; t++) {
+        cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
+        int value;
+        vector<int> values, asc, desc;
+        for (int i = 0; i < n; i++) {
+            cin >> value;
+            values.push_back(value);
+            asc.push_back(1);
+            desc.push_back(1);
+        }
 
-        ascending[0][i] = temp;
-        decreasing[0][i] = temp;
+        int mm = 0;
+        for (int i = asc.size() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < asc.size(); j++) {
+                if (values[i] < values[j]) {
+                    asc[i] = max(asc[i], asc[j] + 1);
+                } else {
+                    desc[i] = max(desc[i], desc[j] + 1);
+                }
+            }
+        }
+        for (int i = 0; i < asc.size(); i++) mm = max(mm, asc[i] + desc[i] - 1);
+        cout << mm << endl;
     }
-    magic_function();
-
-    int max_length = 0;
-    for (int i = 0; i < n; i++) {
-        max_length = max(max_length, ascending[1][i] + decreasing[1][i] - 1);
-    }
-    cout << max_length << endl;
-
     return 0;
 }

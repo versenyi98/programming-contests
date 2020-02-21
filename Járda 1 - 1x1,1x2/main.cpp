e44d055d1f10eab57fc1169e914980b7f87245c9
Péter <versenyi98@gmail.com>
2020-02-21 17:05:37 +0100

    Add Járda 1 - 1x1,1x2

---
+++ Járda 1 - 1x1,1x2/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool taken[81];
+int dp[81];
+
+int n;
+
+int dynamic(int s) {
+
+    if (s == 0) return 1;
+    if (s < 0) return 0;
+    if (taken[s]) return dp[s];
+
+    taken[s] = true;
+    dp[s] = dynamic(s - 1) + dynamic(s - 2);
+    return dp[s];
+}
+
+int main() {
+
+    for (int i = 0; i < 81; i++) {
+        taken[i] = false;
+    }
+
+    cin >> n;
+    cout << dynamic(n) << endl;
+} 

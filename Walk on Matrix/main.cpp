4f7c98bfb2b40d6dd961b6bcaaaea48e7221f7a3
versenyi98 <versenyi98@gmail.com>
2020-04-01 08:32:50 +0200

    Add Walk on Matrix

---
+++ Walk on Matrix/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    uint64_t k;
+    cin >> k;
+    cout << 2 << ' ' << 3 << endl;
+    cout << pow(2, 17) + k << ' ' << k << ' ' << 0 << endl;
+    cout << pow(2, 17) << ' ' << pow(2, 17) + k << ' ' << k << endl;
+}

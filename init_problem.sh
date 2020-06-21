2584734bbd9d7d39a2506da38960483aab9f075c
versenyi98 <versenyi98@gmail.com>
2020-06-21 13:54:34 +0200

    Modify init_problem.sh

--- init_problem.sh
+++ init_problem.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 FOLDER_NAME=$1
+PROBLEM_LINK={$2:-""}
 
 mkdir "$FOLDER_NAME"
 cd "$FOLDER_NAME"
 touch in main.cpp README.md
-cd ..
+echo $2 > README.md
 
-code "$FOLDER_NAME"
+code .

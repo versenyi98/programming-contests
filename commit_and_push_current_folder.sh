1e23750204428839a1352251f29ab73c79a6ad1a
versenyi98 <versenyi98@gmail.com>
2020-08-20 09:39:07 +0200

    Add /home/versenyi/Desktop/programming-contests/UVa Online Judge/13190 - Rockabye Tobby

--- commit_and_push_current_folder.sh
+++ commit_and_push_current_folder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 PWD=$(basename "$(pwd)")
+LAST_DIR=$(pwd)
+
+cd ../../
+./generate_readme.sh > README.md
+cd "$LAST_DIR"
 git add .
-git commit -m "Add $PWD"
+git commit -a -m "Add $PWD"
 git push

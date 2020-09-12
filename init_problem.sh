3bb339cf09037ea46d9647d54aab76af5a1659a0
versenyi98 <versenyi98@gmail.com>
2020-09-12 13:55:23 +0200

    Update init_problem

--- init_problem.sh
+++ init_problem.sh
@@ -5,8 +5,23 @@ PROBLEM_LINK={$2:-""}
 
 mkdir "$FOLDER_NAME"
 cd "$FOLDER_NAME"
+
 touch in main.cpp README.md
+
 echo $2 > README.md
-printf "#include <bits/stdc++.h>\n\nusing namespace std;\n\nint main() {\n\n\treturn 0;\n}" > main.cpp
+
+printf "#include <bits/stdc++.h>
+#define endl \"\\\n\"
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    return 0;
+}" > main.cpp
 
 code ../../ ./*

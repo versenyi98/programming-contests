703347c150def2580d9b7e233e472ce13e7d5f20
versenyi98 <versenyi98@gmail.com>
2020-12-19 14:32:21 +0100

    Add compile.sh

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+# Add the following line to ~/.bashrc:
+# source /path/to/compile.sh
+
+# After this, the terminal should recognize the compile command
+
+function compile() {
+    g++ $PWD/main.cpp -o $PWD/main
+}

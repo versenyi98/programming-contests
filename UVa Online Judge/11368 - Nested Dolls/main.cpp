f83d0133d463f478b9406cc94988b7781906eb4f
versenyi98 <versenyi98@gmail.com>
2021-01-17 13:11:14 +0100

    Add faster solution to 11368 - Nested Dolls

--- UVa Online Judge/11368 - Nested Dolls/main.cpp
+++ UVa Online Judge/11368 - Nested Dolls/main.cpp
@@ -1,55 +1,74 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 
 using namespace std;
 
-vector<pair<int, int>> DOLL(20000);
-int DOLL_SIZE;
+vector<pair<int, int>> dolls;
+vector<int> second;
+int n;
 
-pair<int, int> LIS[20000];
-int LIS_SIZE;
+vector<int> lis_second;
+int lis_size;
 
-int lis[20000];
 int result;
-void LIS_F(int i) {
 
-    int j = 0;
-    while (DOLL[i].second >= LIS[j].second && j < LIS_SIZE) j++;
-    if (j == LIS_SIZE) {
-        LIS[j] = DOLL[i];
-        LIS_SIZE++;
-    } else if (DOLL[i].second <= LIS[j].second) {
-        LIS[j] = DOLL[i];
+void lis_F(int i) {
+
+    int j = lower_bound(lis_second.begin(), lis_second.begin() + lis_size, second[i] + 1) - lis_second.begin();
+
+    if (j == lis_size) {
+        lis_second[j] = second[i];
+        lis_size++;
+    } else if (lis_second[i] <= second[j]) {
+        lis_second[j] = second[i];
     }
-    
+
     result = max(j, result);
 }
 
 int main() {
 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int tc;
     cin >> tc;
-    for (int ttt = 0; ttt < tc; ttt++) {
-        cin >> DOLL_SIZE;
-        result = 0;
-        LIS_SIZE = 0;
 
-        for (int i = 0; i < DOLL_SIZE; i++) {
-            cin >> DOLL[i].first >> DOLL[i].second;
-            lis[i] = 0;
+    for (int tt = 0; tt < tc; tt++) {
+        cin >> n;
+
+        result = 0;
+        lis_size = 0;
+
+        dolls.resize(n);
+        lis_second.assign(n, 0);
+        second.clear();
+
+        for (int i = 0; i < n; i++) {
+            cin >> dolls[i].first >> dolls[i].second;
         }
 
-        sort(DOLL.begin(), DOLL.begin() + DOLL_SIZE, [] (pair<int, int> a, pair<int, int> b) {
+        sort(dolls.begin(), dolls.end(), [] (pair<int, int> a, pair<int, int> b) {
             if (a.first == b.first) {
                 return a.second < b.second;
             }
             return a.first > b.first;
         });
-        for (int i = 0; i < DOLL_SIZE; i++) LIS_F(i);
 
-        if (DOLL_SIZE == 0) {
+        for (auto i : dolls) {
+            second.push_back(i.second);
+        }
+
+        for (int i = 0; i < n; i++) {
+            lis_F(i);
+        }
+
+        if (n == 0) {
             cout << 0 << endl;
             continue;
         }
+
         cout << result + 1  << endl;
     }
 }

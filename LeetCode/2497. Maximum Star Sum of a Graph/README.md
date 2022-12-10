07a18660fe117cd6fc518007114c5119da8c0af7
Péter Versényi <versenyi98@gmail.com>
2022-12-10 19:55:23 +0100

    Add "2497. Maximum Star Sum of a Graph"

---
+++ LeetCode/2497. Maximum Star Sum of a Graph/README.md
@@ -0,0 +1,3 @@
+| ID | Link to description | Link to solution
+|:---|:---|:---:|
+| 2497 | [Maximum Star Sum of a Graph](https://leetcode.com/problems/maximum-star-sum-of-a-graph/) | [Solution](https://github.com/versenyi98/programming-contests/tree/master/LeetCode/2497.%20Maximum%20Star%20Sum%20of%20a%20Graph)|
---
+++ LeetCode/2497. Maximum Star Sum of a Graph/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxStarSum(self, vals: List[int], edges: List[List[int]], k: int) -> int:
+        maximum_star_sum = None
+
+        connected = {}
+        for edge in edges:
+            f, t = edge
+            if not (f in connected):
+                connected[f] = []
+            if not (t in connected):
+                connected[t] = []
+            connected[t].append(f)
+            connected[f].append(t)
+
+        for idx, value in enumerate(vals):
+            if maximum_star_sum is None:
+                maximum_star_sum = value
+            current_star_sum = value
+            if idx in connected:
+                neighbours = sorted([vals[other_idx] for other_idx in connected[idx]], reverse=True)
+                current_size = 0
+                while current_size < len(neighbours) and current_size != k and neighbours[current_size] >= 0:
+                    print(current_size, neighbours[current_size])
+                    current_star_sum += neighbours[current_size]
+                    current_size += 1
+            maximum_star_sum = max(current_star_sum, maximum_star_sum)
+
+        return maximum_star_sum

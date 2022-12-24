aa81bd869654ad773fa83bea615cd14227b9414a
Péter Versényi <versenyi98@gmail.com>
2022-12-24 20:26:54 +0100

    Add "2512. Reward Top K Students"

---
+++ LeetCode/2512. Reward Top K Students/README.md
@@ -0,0 +1,3 @@
+| ID | Link to description | Link to solution
+|:---|:---|:---:|
+| 2512 | [Reward Top K Students](https://leetcode.com/problems/reward-top-k-students/) | [Solution](https://github.com/versenyi98/programming-contests/tree/master/LeetCode/2512.%20Reward%20Top%20K%20Students)|
---
+++ LeetCode/2512. Reward Top K Students/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topStudents(self, positive_feedback: List[str], negative_feedback: List[str], report: List[str],
+                    student_id: List[int], k: int) -> List[int]:
+        positive_feedback = set(positive_feedback)
+        negative_feedback = set(negative_feedback)
+
+        print(positive_feedback)
+        print(negative_feedback)
+
+        points = {id: (0, id) for id in student_id}
+
+        for idx, rep in enumerate(report):
+            print(idx, rep)
+            current_points = points[student_id[idx]]
+
+            for word in rep.split():
+                if word in negative_feedback:
+                    current_points = (current_points[0] - 1, current_points[1])
+                if word in positive_feedback:
+                    current_points = (current_points[0] + 3, current_points[1])
+            points[student_id[idx]] = current_points
+
+        results = sorted(points.values(), key=lambda x: (-x[0], x[1]))[:k]
+
+        return list(map(lambda x: x[1], results))

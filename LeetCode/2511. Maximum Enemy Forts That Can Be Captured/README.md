1746f8db611e0cd42ac780e1fbd0a770fa315fa1
Péter Versényi <versenyi98@gmail.com>
2022-12-24 20:27:44 +0100

    Add "2511. Maximum Enemy Forts That Can Be Captured"

---
+++ LeetCode/2511. Maximum Enemy Forts That Can Be Captured/README.md
@@ -0,0 +1,3 @@
+| ID | Link to description | Link to solution
+|:---|:---|:---:|
+| 2511 | [Maximum Enemy Forts That Can Be Captured](https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/) | [Solution](https://github.com/versenyi98/programming-contests/tree/master/LeetCode/2511.%20Maximum%20Enemy%20Forts%20That%20Can%20Be%20Captured)|
---
+++ LeetCode/2511. Maximum Enemy Forts That Can Be Captured/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def captureForts(self, forts: List[int]) -> int:
+        maximum = 0
+
+        from_fort1 = -1
+        from_fort2 = -1
+        for idx in range(len(forts)):
+            ffort = forts[idx]
+            bfort = forts[len(forts) - 1 - idx]
+            print(idx, ffort, bfort)
+            if ffort == -1:
+                if from_fort1 != -1:
+                    maximum = max(maximum, idx - from_fort1 - 1)
+                from_fort1 = -1
+            elif ffort == 1:
+                from_fort1 = idx
+
+            if bfort == -1:
+                print("Bfort", from_fort2, idx)
+                if from_fort2 != -1:
+                    maximum = max(maximum, from_fort2 - (len(forts) - 1 - idx) - 1)
+                from_fort2 = -1
+            elif bfort == 1:
+                from_fort2 = len(forts) - 1 - idx
+        return maximum

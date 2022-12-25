a54b895dd10accd8593ff697363ae9d0e8fa88a9
Péter Versényi <versenyi98@gmail.com>
2022-12-26 00:51:42 +0100

    Add "10154 - Weights and Measures"

---
+++ UVa Online Judge/10154 - Weights and Measures/README.md
@@ -0,0 +1,3 @@
+| ID | UVa Online Judge | External | Link to solution |
+|:---|:---|:---|:---:|
+| 10154 | [Weights and Measures](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=13&page=show_problem&problem=1095) | [PDF](https://onlinejudge.org/external/101/10154.pdf) | [Solution](https://github.com/versenyi98/programming-contests/tree/master/UVa%20Online%20Judge/10154%20-%20Weights%20and%20Measures)|
---
+++ UVa Online Judge/10154 - Weights and Measures/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    long w, s;
+    vector<pair<long, long>> turtles;
+    while (cin >> w >> s) {
+        turtles.push_back({w, s});
+    }
+
+    vector<vector<long>> weights(turtles.size());
+    sort(turtles.begin(), turtles.end(), [](pair<int, int> f, pair<int, int> s) {
+        return (f.second == s.second && f.first < s.first) || f.second < s.second;
+    });
+
+    for (auto turtle : turtles) {
+        //cout << turtle.first << " " << turtle.second << endl;
+    }
+
+    for (int i = 0; i < turtles.size(); i++) {
+        weights[i] = vector<long>(i + 1, INT_MAX);
+    }
+    weights[0][0] = turtles[0].first;
+    
+    for (int i = 1; i < turtles.size(); i++) {
+        weights[i][0] = min(weights[i - 1][0], turtles[i].first);
+    }
+
+    int result = 1;
+    for (int i = 1; i < turtles.size(); i++) {
+        for (int j = 1; j < i + 1; j++) {
+            if (i - 1 >= j) {
+                weights[i][j] = weights[i - 1][j];
+            }
+            if (weights[i - 1][j - 1] + turtles[i].first <= turtles[i].second) {
+                result = max(result, j + 1);
+                weights[i][j] = min(weights[i - 1][j - 1] + turtles[i].first, weights[i][j]);
+            }
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
